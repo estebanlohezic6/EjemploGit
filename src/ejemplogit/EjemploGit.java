9435ec70c63eb91b0d654e5192a6d2a534ba79fe
estebanlohezic6 <estebanlohezi@gmail.com>
2018-10-02 16:37:28 -0300

    Primer cambio al proyecto.

--- src/ejemplogit/EjemploGit.java
+++ src/ejemplogit/EjemploGit.java
@@ -3,7 +3,8 @@ package ejemplogit;
 public class EjemploGit {
 
     public static void main(String[] args) {
-        
+        System.out.println("El tanuki está pescando.");
+        System.out.println("Es perturbador.");
     }
     
 }
